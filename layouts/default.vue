441f22f6eadec212b79cc04b8780f5f16ce42276
Lokesh Parmar <lokesh.parmar998@gmail.com>
2022-05-27 17:55:46 +0530

    android added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,13 @@
 <template>
   <div>
-    <Header />
-    <div class="default">
-      <div class="side-nav">
-        <SideNav />
-      </div>
-      <Nuxt class="main" />
-    </div>
+    <Nuxt />
   </div>
 </template>
 <script>
-import Header from "@/components/navigation/Header.vue";
-import SideNav from "@/components/navigation/SideNav.vue";
 export default {
   name: "DefaultLayout",
-  middleware: "auth",
-  components: {
-    Header,
-    SideNav,
-  },
+  /* middleware: "auth", */
 };
 </script>
 <style scoped>
-.default {
-  display: flex;
-  width: 80%;
-  margin: auto;
-}
-.main {
-  width: 85%;
-}
-.side-nav {
-  width: 15%;
-}
 </style>
